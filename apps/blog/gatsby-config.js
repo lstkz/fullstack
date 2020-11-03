354cbd8d0a012d17d23dc76d95abb3b3afbb1933
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-11-03 17:18:49 +0100

    add google-analytics

--- apps/blog/gatsby-config.js
+++ apps/blog/gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-SND3BFVW8M`,
       },
     },
     `gatsby-plugin-feed`,
