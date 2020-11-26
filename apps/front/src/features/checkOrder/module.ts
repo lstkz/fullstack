57be440cf34a33c16567e8fa592283d468e504f2
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-11-26 16:24:53 +0100

    tsc, test fix

--- apps/front/src/features/checkOrder/module.ts
+++ apps/front/src/features/checkOrder/module.ts
@@ -1,9 +1,9 @@
-import { api } from 'src/services/api';
+import * as Rx from 'src/rx';
 import { CheckOrderActions, CheckOrderState, handle } from './interface';
 
 // --- Epic ---
 handle.epic().on(CheckOrderActions.$mounted, () => {
-  api.get;
+  return Rx.EMPTY;
 });
 
 // --- Reducer ---
