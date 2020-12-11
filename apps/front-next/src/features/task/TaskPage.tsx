3b01785da80a7dd66a86d249acd01cfc3c4bc160
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-12-11 17:56:05 +0100

    splitter

--- apps/front-next/src/features/task/TaskPage.tsx
+++ apps/front-next/src/features/task/TaskPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ModuleTaskDetails } from 'shared';
 import { Loader } from 'src/components/Loader';
 import { TaskHeader } from './TaskHeader';
+import SplitPane from 'react-split-pane';
 
 interface TaskPageProps {
   task: ModuleTaskDetails;
@@ -31,34 +32,57 @@ export function TaskPage(props: TaskPageProps) {
       script.remove();
     };
   }, []);
+  const [isDragging, setIsDragging] = React.useState(false);
+
+  const defaultSize = React.useMemo(() => {
+    if (typeof localStorage === 'undefined') {
+      return 300;
+    }
+    return Number(localStorage.taskPaneWidth) || 300;
+  }, []);
 
   const renderDetails = () => {
     if (!details) {
       return <Loader />;
     }
     return (
-      <div className="flex h-full" style={{ minWidth: 1000 }}>
-        <div className="bg-white p-4 h-full" style={{ width: 400 }}>
-          {details}
-        </div>
+      <SplitPane
+        split="vertical"
+        minSize={50}
+        defaultSize={defaultSize}
+        onDragStarted={() => {
+          setIsDragging(true);
+        }}
+        onDragFinished={newSize => {
+          localStorage.taskPaneWidth = newSize;
+          setIsDragging(false);
+        }}
+        resizerStyle={{
+          width: 5,
+          cursor: 'col-resize',
+          background: 'white',
+        }}
+      >
+        <div className="bg-white p-4 h-full">{details}</div>
         <div className="h-full flex-1">
           <iframe
             style={{
               width: '100%',
               height: '100%',
               border: 0,
+              pointerEvents: isDragging ? 'none' : undefined,
             }}
             src="https://test-vm.styx-dev.com/?folder=/home/ubuntu/task1"
           />
         </div>
-      </div>
+      </SplitPane>
     );
   };
 
   return (
     <div className="flex h-full flex-col">
       <TaskHeader />
-      {renderDetails()}
+      <div className="flex-1 relative">{renderDetails()}</div>
     </div>
   );
 }
