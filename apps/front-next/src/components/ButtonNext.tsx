64e93c32b22f037e5d2eec145c4ac86bb4803f32
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-12-11 14:25:24 +0100

    wip

---
+++ apps/front-next/src/components/ButtonNext.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import Link from 'next/link';
+import { SpinnerBoarder } from './SpinnerBoarder';
+import classNames from 'classnames';
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  className?: string;
+  block?: boolean;
+  soft?: boolean;
+  outline?: boolean;
+  loading?: boolean;
+  type: 'primary' | 'secondary' | 'danger' | 'dark' | 'neutral' | 'warning';
+  size?: 'extra-small' | 'small' | 'default' | 'large' | 'extra-large';
+  href?: string;
+  icon?: React.ReactNode;
+  disabled?: boolean;
+  htmlType?: 'button' | 'submit' | 'reset';
+  onClick?: (
+    e: React.MouseEvent<HTMLAnchorElement | HTMLDivElement, MouseEvent>
+  ) => void;
+  testId?: string;
+}
+
+export const ButtonNext = React.forwardRef((props: ButtonProps, ref: any) => {
+  const {
+    className,
+    onClick,
+    children,
+    htmlType,
+    loading,
+    disabled,
+    testId,
+    icon,
+    href,
+    type,
+  } = props;
+  const inner = (
+    <>
+      {(loading || icon) && (
+        <div className="mr-3">
+          {loading ? <SpinnerBoarder size="sm" /> : icon}
+        </div>
+      )}
+      {children}
+    </>
+  );
+  const classList = [
+    'inline-flex item-center justify-center transition-all rounded-md border border-transparent font-semibold py-3 px-7 leading-normal focus:ring outline-none',
+  ];
+
+  switch (type) {
+    case 'primary':
+      break;
+    case 'secondary':
+      classList.push(
+        'text-gray-900 bg-secondary border-color-secondary',
+        'hover:bg-secondary-600 hover:border-color-secondary-700',
+        'active:bg-secondary-700 active:border-color-secondary-800',
+        'focus:ring-secondary-ring'
+      );
+      break;
+    case 'warning':
+      break;
+    case 'neutral':
+      break;
+  }
+  if (href) {
+    return (
+      <Link href={href}>
+        <a
+          data-test={testId}
+          onClick={onClick as any}
+          className={classNames(classList, className)}
+          ref={ref}
+        >
+          {inner}
+        </a>
+      </Link>
+    );
+  }
+  return (
+    <button
+      data-test={testId}
+      disabled={loading || disabled}
+      onClick={onClick as any}
+      className={classNames(classList, className)}
+      type={htmlType || 'button'}
+      ref={ref}
+    >
+      {inner}
+    </button>
+  );
+});
--- apps/front-next/src/features/modules/ModuleInfo.tsx
+++ apps/front-next/src/features/modules/ModuleInfo.tsx
@@ -2,121 +2,48 @@ import {
   faCode,
   faGraduationCap,
   faHourglassHalf,
+  IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { createUrl } from 'src/common/url';
 import { TsIcon } from 'src/icons/TsIcon';
 import { Button } from 'src/components/Button';
-import { Col, Row } from 'src/components/Grid';
-import { Heading } from 'src/components/Heading';
 import { ProgressBar } from 'src/components/ProgressBar';
-import { Theme } from 'src/Theme';
-import styled from 'styled-components';
 import { Module } from 'shared';
 import Link from 'next/link';
+import { HeadingNext } from 'src/components/HeadingNext';
+import { ButtonNext } from 'src/components/ButtonNext';
 
 interface ModuleInfoProps {
   className?: string;
   module: Module;
 }
 
-interface WrapperProps {}
+interface StatProps {
+  icon: IconDefinition;
+  text: React.ReactNode;
+}
 
-const Stat = styled.div`
-  margin: 0 0.5rem;
-`;
-
-const Wrapper = styled<WrapperProps, 'div'>('div')`
-  display: block;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid ${Theme.gray_200};
-  border-radius: 0.75rem;
-  padding: 1.75rem 1.75rem 0;
-  color: ${Theme.gray_600};
-`;
-
-const Footer = styled.div`
-  padding: 1rem 0;
-  position: relative;
-  margin-top: 1rem;
-  font-size: 0.75rem;
-  font-weight: 300;
-  color: ${Theme.gray_600};
-
-  svg {
-    color: ${Theme.primary};
-  }
-  &:before {
-    content: '';
-    display: block;
-    width: 80%;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    margin-left: -40%;
-    height: 1px;
-    background: radial-gradient(
-      ellipse at center,
-      #d1dbe7 0,
-      rgba(255, 255, 255, 0) 75%
-    );
-  }
-`;
-
-const StatsCol = styled(Col)`
-  display: flex;
-  align-items: flex-end;
-`;
-
-const Col1 = styled(Col)``;
-
-const ButtonCol = styled(Col)`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-const Icon = styled.div`
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 1rem;
-  svg {
-    width: 2rem;
-    height: 2rem;
-  }
-`;
-
-const Title = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-`;
+function Stat({ icon, text }: StatProps) {
+  return (
+    <div className="mx-2">
+      <FontAwesomeIcon icon={icon} className="text-blue" /> {text}
+    </div>
+  );
+}
 
 export const ModuleInfo = (props: ModuleInfoProps) => {
   const { module } = props;
-  const [, setTick] = React.useState(0);
-  React.useEffect(() => {
-    let nextTick = 1;
-    const id = setInterval(() => {
-      setTick(nextTick++);
-    }, 500);
-    return () => {
-      clearInterval(id);
-    };
-  }, []);
 
   return (
-    <Wrapper>
-      <Row>
-        <Col1 md={10}>
-          <Title>
-            <Icon>
-              <TsIcon />
-            </Icon>
+    <div className="rounded-xl bg-white p-7 pb-0 border border-gray-200 shadow-sm">
+      <div className="md:grid grid-cols-6">
+        <div className="col-span-5">
+          <div className="flex items-center mb-4">
+            <div className="flex items-center justify-center mr-4">
+              <TsIcon className="w-8 h-8" />
+            </div>
             <Link
               href={createUrl({
                 name: 'module',
@@ -124,39 +51,33 @@ export const ModuleInfo = (props: ModuleInfoProps) => {
               })}
               data-test="title"
             >
-              <Heading type={5}>{module.name}</Heading>
+              <HeadingNext type={5}>{module.name}</HeadingNext>
             </Link>
-          </Title>
+          </div>
           <div>{module.description}</div>
-        </Col1>
-        <ButtonCol md={2}>
-          <Button
+        </div>
+        <div className="flex justify-end items-center mt-4 md:m-0">
+          <ButtonNext
             testId="show-btn"
             type="secondary"
             href={createUrl({ name: 'module', id: module.id })}
           >
             Pokaż
-          </Button>
-        </ButtonCol>
-      </Row>
-      <Footer>
-        <Row>
-          <StatsCol md={6}>
-            <Stat>
-              <FontAwesomeIcon icon={faGraduationCap} /> 20 lekcji
-            </Stat>
-            <Stat>
-              <FontAwesomeIcon icon={faCode} /> 30 zadań
-            </Stat>
-            <Stat>
-              <FontAwesomeIcon icon={faHourglassHalf} /> 20 godzin praktyki
-            </Stat>
-          </StatsCol>
-          <Col md={6}>
+          </ButtonNext>
+        </div>
+      </div>
+      <div className="sep py-4 mt-4 text-xs text-gray-600 font-light">
+        <div className="md:grid grid-cols-2">
+          <div className="flex items-end">
+            <Stat icon={faGraduationCap} text="20 lekcji" />
+            <Stat icon={faCode} text="30 zadań" />
+            <Stat icon={faHourglassHalf} text="20 godzin praktyki" />
+          </div>
+          <div className="mt-4 md:m-0">
             <ProgressBar title="Postęp" progress={25} />
-          </Col>
-        </Row>
-      </Footer>
-    </Wrapper>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
