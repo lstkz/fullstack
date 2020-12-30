7fda7fdb5d0463e984df0f612839147ff4f7f1cc
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-12-30 15:37:20 +0100

    check order

--- apps/front-next/src/components/SpinnerBoarder.tsx
+++ apps/front-next/src/components/SpinnerBoarder.tsx
@@ -1,41 +1,24 @@
+import classNames from 'classnames';
 import * as React from 'react';
-import styled, { css, keyframes } from 'styled-components';
 
 interface SpinnerBoarderProps {
   className?: string;
   size?: 'sm';
 }
 
-const _SpinnerBoarder = (props: SpinnerBoarderProps) => {
-  const { className } = props;
-  return <div className={className} />;
-};
-
-const rotateAnim = keyframes`
-  to {
-    transform: rotate(360deg); 
-  }  
-`;
-
-export const SpinnerBoarder = styled(_SpinnerBoarder)`
-  display: inline-block;
-  width: 2rem;
-  height: 2rem;
-  vertical-align: text-bottom;
-  border: 0.25em solid currentColor;
-  border-right-color: transparent;
-  border-radius: 50%;
-  animation: ${rotateAnim} 0.75s linear infinite;
-  ${props => {
-    switch (props.size) {
-      case 'sm':
-        return css`
-          width: 1rem;
-          height: 1rem;
-          border-width: 0.2em;
-        `;
-      default:
-        return null;
-    }
-  }}
-`;
+export function SpinnerBoarder(props: SpinnerBoarderProps) {
+  const { className, size } = props;
+  return (
+    <div
+      className={classNames(
+        className,
+        'inline-block border-current rounded-full animate-spin',
+        size === 'sm' && 'w-4 h-4 border',
+        !size && 'w-8 h-8 border-4'
+      )}
+      style={{
+        borderRightColor: 'transparent',
+      }}
+    />
+  );
+}
