15e543a271457ac7b9ea6d165c2436564f9d42c8
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-01-03 15:52:19 +0100

    fix

--- content/modules/1-ts-basics/task-9/source/__tests__/main.test.ts
+++ content/modules/1-ts-basics/task-9/source/__tests__/main.test.ts
@@ -2,11 +2,11 @@ import { missingNumber } from '../src/main';
 
 test.each([
   [[1, 1, 2, null], 6, 2],
-  [[1, 1, , null, 10, 2, 2], 26, 10],
+  [[1, 1, null, 10, 2, 2], 26, 10],
   [[5, null], 10, 5],
   [[null, 1, 2, 3, 2, 3], 12, 1],
   [[null, 1000, 1000, 999, 999, 1], 4000, 1],
   [[null, 1000, 1000, 999, 999, 998, 998, 997], 7988, 997],
 ])('.missingNumber(%p, %p)', (a, b, expected) => {
-  expect(missingNumber(a as Array<number | null>, b)).toEqual(expected);
+  expect(missingNumber(a, b)).toEqual(expected);
 });
