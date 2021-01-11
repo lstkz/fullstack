5e65500a10facd2973c8e266278f2a6bf5176d23
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-01-11 15:34:43 +0100

    hints

--- content/modules/1-ts-basics/task-8/details/hint.tsx
+++ content/modules/1-ts-basics/task-8/details/hint.tsx
@@ -2,5 +2,25 @@ import React from 'react';
 import { Code } from 'ui';
 
 export function Hint() {
-  return <div>TODO</div>;
+  return (
+    <div>
+      Żeby zaokrąglić liczbę do p-tego miejsca po przecinku, musisz pomnożyć ją
+      przez{' '}
+      <Code>
+        10<sup>p</sup>
+      </Code>
+      , wywołać <Code>Math.round</Code>, a następnie podzielić wynik przez{' '}
+      <Code>
+        10<sup>p</sup>
+      </Code>
+      .
+      <br />
+      <br />
+      <Code>
+        10<sup>p</sup>
+      </Code>{' '}
+      - oznacza 10 do potęgi p-tej. Potęgę możesz obliczyć za pomocą operatora{' '}
+      <Code>**</Code>.
+    </div>
+  );
 }
