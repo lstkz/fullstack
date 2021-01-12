386fabe6aa1bb4dda5bc7c4886fbbe0867fffbcb
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-01-12 12:15:38 +0100

    task 12

--- content/modules/1-ts-basics/task-12/details/hint.tsx
+++ content/modules/1-ts-basics/task-12/details/hint.tsx
@@ -5,10 +5,10 @@ export function Hint() {
   return (
     <div>
       Iteruj po każdym piętrze i obliczaj sumę od ziemi do tego piętra. Jeżeli
-      obecna suma jest więszka od danej wysokości to znaczy, że iterowane piętro
-      zawiera tę wysokość. Trzeba również sprawdzić dodatkowy warunek, czy dana
-      wysokość jest równa wysokości całego budynku. Jeżeli tak, to znaczy na
-      ostatnie piętro znajduję się na danej wysokości.
+      obecna suma jest większa od danej wysokości, to znaczy, że iterowane
+      piętro zawiera tę wysokość. Trzeba również sprawdzić dodatkowy warunek,
+      czy dana wysokość jest równa wysokości całego budynku. Jeżeli tak, to
+      znaczy, że ostatnie piętro znajduję się na danej wysokości.
     </div>
   );
 }
