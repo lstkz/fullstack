30611a6324c0891fef30727d074c19e16cc55609
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-01-12 17:04:43 +0100

    task 13 hint

---
+++ content/modules/1-ts-basics/task-13/details/hint.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Code } from 'ui';
+
+export function Hint() {
+  return (
+    <div>
+      Zrób trzy pętle <Code>for</Code> ze zmiennymi <Code>i, j, k</Code>. Jeżeli
+      warunki są spełnione, dodaj trójkę do wyniku.
+    </div>
+  );
+}
